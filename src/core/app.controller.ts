4879152be296a0230a9d8e6d81429b37bb744024
teslo <teslo.knife@gmail.com>
2026-09-20 22:35:15 +0500

    Настройка первых endpoint api

--- src/core/app.controller.ts
+++ src/core/app.controller.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service.js'
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	public constructor(private readonly appService: AppService) {}
 
 	@Get()
-	getHello(): string {
+	public getHello() {
 		return this.appService.getHello()
+	}
+
+	@Get('health')
+	public health() {
+		return this.appService.health()
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
-import { AppModule } from './app.module.js'
+import { AppModule } from './core/app.module.js'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
