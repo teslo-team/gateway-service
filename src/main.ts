40dbc1eeb6bc02ae47c04d0707f8bb1d40536ffc
teslo <teslo.knife@gmail.com>
2026-09-20 22:16:37 +0500

    после форматирования

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module.js";
-import { Logger } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module.js'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule)
 
-  const config = app.get(ConfigService);
-  const logger = new Logger();
+	const config = app.get(ConfigService)
+	const logger = new Logger()
 
-  app.enableCors({
-    origin: config.getOrThrow<string>("HTTP_CORS").split(","),
-    credentials: true,
-  });
+	app.enableCors({
+		origin: config.getOrThrow<string>('HTTP_CORS').split(','),
+		credentials: true
+	})
 
-  await app.listen(process.env.PORT ?? 3000);
+	const port = config.getOrThrow<number>('HTTP_PORT')
+	const host = config.getOrThrow<string>('HTTP_HOST')
+
+	await app.listen(port)
+
+	logger.log(`Gateway started: ${host}`)
+	logger.log(`Swaager: ${host}/docs`)
 }
-await bootstrap();
+await bootstrap()
