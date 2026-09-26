c9fbd84eb36c0fa7ed0aa135bdbebf270cef940f
teslo <teslo.knife@gmail.com>
2026-09-26 11:09:36 +0500

    Доделан endpoint SendOtpRequest

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,15 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { ApiOperation } from '@nestjs/swagger'
 
 import { SendOtpRequest } from './dto'
 
 @Controller('auth')
 export class AuthController {
+	@ApiOperation({
+		summary: 'Send otp code',
+		description:
+			'Sends a verification code to the user phone number or email.'
+	})
 	@Post('otp/send')
 	@HttpCode(HttpStatus.OK)
 	public async sendOtp(@Body() dto: SendOtpRequest) {
--- src/modules/auth/dto/requests/send-otp.request.ts
+++ src/modules/auth/dto/requests/send-otp.request.ts
@@ -1,11 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEnum, IsString, Validate } from 'class-validator'
 import { IdentifierValidator } from 'src/shared/validators'
 
 export class SendOtpRequest {
+	@ApiProperty({
+		example: '+79119224701'
+	})
 	@IsString()
 	@Validate(IdentifierValidator)
 	public identifier: string
 
+	@ApiProperty({
+		example: 'phone',
+		enum: ['phone', 'email']
+	})
 	@IsEnum(['phone', 'email'])
 	public type: 'phone' | 'email'
 }
